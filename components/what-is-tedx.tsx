21afc8c54352e5552d3785e403bb82afd437d26b
Ukevine <uwasek21@gmail.com>
2025-08-14 19:41:49 +0200

    Add my improvements

--- components/what-is-tedx.tsx
+++ components/what-is-tedx.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function WhatIsTedx() {
-  const ref = useRef(null);
+  const ref = useRef<HTMLElement>(null);
   const isInView = useInView(ref, { once: true, amount: 0.3 });
 
   return (
